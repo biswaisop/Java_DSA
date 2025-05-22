c36a0f8d9d49c165246fe6dea140c69d8012a966
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2025-05-22 23:27:24 +0530

    second

--- Arrays.java
+++ Arrays.java
@@ -3,41 +3,41 @@ import java.util.Scanner;
 public class Arrays {
     public static void main(String[] args) {
         // one-D arrays
-//        int [] marks = new int[3]; // first method to define an array
-//        marks[0] = 97;
-//        marks[1] = 98;
-//        marks[2] = 99;
-//        System.out.println(marks);, will print some value that wil have the mixture of class name and hash codes
-//        int[] nums = {1,2,3,4,}; // second method to define an array
+        int [] marks = new int[3]; // first method to define an array
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 99;
+        System.out.println(marks); //will print some value that wil have the mixture of class name and hash codes
+        int[] nums = {1,2,3,4,}; // second method to define an array
 //
 //        taking size input from user
         Scanner sc = new Scanner(System.in);
 //
-//        int size = sc.nextInt();
-//        int  [] numbers = new int[size];
-//
-//        for(int i = 0; i < size; i++){
-//            numbers[i] = sc.nextInt();
-//        }
-//        for(int i = 0; i < size; i++){
-//            System.out.println(numbers[i]);
-//        }
+        int size = sc.nextInt();
+        int  [] numbers = new int[size];
+
+        for(int i = 0; i < size; i++){
+            numbers[i] = sc.nextInt();
+        }
+        for(int i = 0; i < size; i++){
+            System.out.println(numbers[i]);
+        }
 //
         //2-d arraus
         System.out.print("Enter the number of rows: ");int rows = sc.nextInt();
         System.out.print("Enter the number of rows: ");int cols = sc.nextInt();
-        int[][] numbers = new int[rows][cols];
+        int[][] numbers2 = new int[rows][cols];
         System.out.println("Enter the elements: ");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                numbers[i][j] = sc.nextInt();
+                numbers2[i][j] = sc.nextInt();
             }
         }
 
         System.out.println("The array is: ");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                System.out.print(numbers[i][j] + " ");
+                System.out.print(numbers2[i][j] + " ");
             }
             System.out.println();
         }
