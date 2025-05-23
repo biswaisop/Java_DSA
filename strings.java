efa4dc60cbe264b5e63218550372ae15a7f23f83
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2025-05-23 17:56:52 +0530

    third

---
+++ strings.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class strings {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    //System.out.print("Enter Your Name: ");String name2 = sc.next();
+    //System.out.println("My name is "+name2);
+    //String manipulation functions
+
+
+    //Concatenation -> joins two strings
+    String FirstName = "Tony";
+    String LastName = "StarK";
+    String FullName = FirstName + " " + LastName;
+    System.out.println(FullName);
+
+    //Length -> gives the length of the function
+    System.out.println(FullName.length());
+
+    //charAt -> gives the character present at the passed index
+    for (int i = 0; i<FullName.length(); i++){
+        System.out.println(FullName.charAt(i));
+
+    }
+
+    //Comparison -> compares two strings, returns +ve value if s1>s2, 0 if s1==s2, -ve value if s2<s2 on the basis of length
+    String string1 = "tony";
+    String string2 = "stark";
+    if (string1.compareTo(string2) == 0){
+        System.out.println("Equal");
+    }
+    else{
+        if (string1.compareTo(string2) >= 0){
+            System.out.println("s1 is greater");
+        }
+        else{
+            System.out.println("s2 is greater");
+        }
+    }
+
+
+
+
+
+    }
+
+}
