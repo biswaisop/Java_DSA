e0f4998fb8dcfdc22d5df77b2baab7766ea94b4c
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2025-05-25 23:23:10 +0530

    select-sort

--- DSA/BubbSort.java
+++ DSA/BubbSort.java
@@ -1,23 +1,25 @@
 package DSA;
+import java.util.Arrays;
 
 public class BubbSort {
 
-    //Bubble sort
-    public static void Bubbsort(int [] a){
-        for (int i = 0; i < a.length; i++){
-            if (a[i]>a[i+1]){
-                int temp = a[i];
-                a[i] = a[i+1];
-                a[i+1] = temp;
+    // Proper Bubble sort implementation
+    static void Bubbsort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
             }
         }
     }
 
     public static void main(String[] args) {
-        int [] nums = {3,2,4,2,1,4,5,5,5,6,3,65,7,57,9,0};
+        int[] nums = {3, 2, 4, 2, 1, 4, 5, 5, 5, 6, 3, 65, 7, 57, 9, 0};
         Bubbsort(nums);
-        System.out.println(nums);
+        System.out.println(Arrays.toString(nums));
     }
-
-
 }
---
+++ DSA/selectSort.java
@@ -0,0 +1,27 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class selectSort {
+    static void sort(int arr[], int n){
+        for(int i = 0; i < n-1; i++){
+            int mini = i;
+            for(int j = i+1; j < n; j++){
+                if(arr[mini] > arr[j]) {
+                    mini = j;
+                }
+            }
+            int temp = arr[i];
+            arr [i] = arr[mini];
+            arr[mini] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {67,89,23,56,88,65,9,34,56};
+        System.out.println(Arrays.toString(arr));
+        sort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
