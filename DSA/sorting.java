4fcd2e9235ab154e6955d57005b7212f35ec5a14
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2025-05-25 17:54:51 +0530

    sorting

---
+++ DSA/sorting.java
@@ -0,0 +1,23 @@
+package DSA;
+
+public class sorting {
+
+    //Bubble sort
+    public static void Bubbsort(int [] a){
+        for (int i = 0; i < a.length; i++){
+            if (a[i]>a[i+1]){
+                int temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {3,2,4,2,1,4,5,5,5,6,3,65,7,57,9,0};
+        Bubbsort(nums);
+        System.out.println(nums);
+    }
+
+
+}
